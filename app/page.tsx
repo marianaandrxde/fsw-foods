48877f64dff0a3132f0055d45f46b2858cab64a7
Mariana <mariana.olivandrade@gmail.com>
2024-05-01 10:08:32 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Search from "./_components/ui/search";
 import Header from "./_components/header";
 
 const  Home = () => {
   return (
-    <Header/>
+    <>    
+    <Header />
+    <div className="px-5 pt-6">
+        <Search />
+    </div>
+    </>
   );
 };
-
 
  
 export default Home;
