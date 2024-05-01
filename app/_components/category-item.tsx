7b5d9607a90d28ee1b4f89efe486e17a37345c28
Mariana <mariana.olivandrade@gmail.com>
2024-05-01 20:45:49 -0300

    feat: add promo banner to homepage

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,23 +1,19 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
 
-interface CategoryItemProps {
-  category: Category;
+interface CategoryItemProps{
+    category: Category;
 }
 
-const CategoryItem = ({ category }: CategoryItemProps) => {
-  return (
-    <div className="flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
-      <Image
-        src={category.imageUrl}
-        alt={category.name}
-        height={30}
-        width={30}
-      />
 
-      <span className="text-semibold font-semibold">{category.name}</span>
-    </div>
-  );
-};
+const CategoryItem = ({category}: CategoryItemProps) => {
+    return ( 
+    <div className="flex items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
+        <Image src={category.imageUrl} alt={category.name} height={30} width={30} />
 
+        <span className="font-semibold text-semibold">{category.name}</span>
+    </div> 
+    );
+}
+ 
 export default CategoryItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
 import Search from "./_components/search";
 import Header from "./_components/header";
 import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
-const  Home = () => {
+const Home = () => {
   return (
-    <>    
-    <Header />
-    
-    <div className="px-5 pt-6">
+    <>
+      <Header />
+
+      <div className="px-5 pt-6">
         <Search />
-    </div>
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
 
     <div className="px-5 pt-6">
-    <CategoryList />
+    <Image
+        src="/promo-banner-01.png"
+        alt="Até 30% de desconto em pizza"
+        height={0}
+        width={0}
+        sizes="100vw"
+        className="h-auto w-full object-contain"
+        quality={100}
+      />
     </div>
-
     </>
   );
 };
 
- 
 export default Home;
