573bf1a31af2058d6e7b5f407d879c32d9aa15a0
Mariana <mariana.olivandrade@gmail.com>
2024-05-06 20:30:53 -0300

    feat: display of products by category

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,4 +1,5 @@
 import { Category } from "@prisma/client";
+import Link from "next/link";
 import Image from "next/image";
 
 interface CategoryItemProps{
@@ -8,12 +9,14 @@ interface CategoryItemProps{
 
 const CategoryItem = ({category}: CategoryItemProps) => {
     return ( 
+    <Link href={`/categories/${category.id}/products`}>
     <div className="flex items-center gap-3 py-3 px-4 bg-white shadow-md rounded-full">
         <Image src={category.imageUrl} alt={category.name} height={30} width={30} />
 
         <span className="font-semibold text-semibold">{category.name}</span>
     </div> 
+    </Link>
     );
-}
+};
  
 export default CategoryItem;
--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,13 +5,11 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <>
       <div className="grid grid-cols-2 gap-3">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
         ))}
       </div>
-    </>
   );
 };
 
